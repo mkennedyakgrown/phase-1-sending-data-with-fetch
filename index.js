1dcdfbe9cd8d068bf644e4e3f054f33a565ebea8
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2023-06-29 14:40:59 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const postData = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": `${userName}`,
+            "email": `${userEmail}`
+        })
+    };
+    return fetch("http://localhost:3000/users", postData)
+    .then(res => res.json())
+    .then(newUser => {
+        document.body.append(`${newUser.id}`);
+    })
+    .catch(function (error) {
+        document.body.append(`${error.message}`);
+    })
+}
